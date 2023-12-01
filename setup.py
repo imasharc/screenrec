4664c4be36be0a0017e3c2ee317c775ff9859dc4
Antoni Malinowski <antonimalinowski.kontakt@gmail.com>
2023-12-01 16:40:13 +0100

    install dependencies for main.py
    This script is simple python program that installs, using pip, required libraries for main.py to work

---
+++ setup.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+
+def install_libraries():
+    try:
+        # Install required Python libraries
+        subprocess.run([sys.executable, "-m", "pip", "install", "opencv-python", "pyautogui", "Pillow", "screeninfo"])
+
+        print("Libraries installed successfully!")
+
+    except Exception as e:
+        print(f"Error installing libraries: {e}")
+
+if __name__ == "__main__":
+    install_libraries()
